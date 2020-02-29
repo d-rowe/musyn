98ff96f679bc49f1c1308197360c73df224b2415
Daniel Rowe <danielrowetech@gmail.com>
2020-02-29 15:57:11 -0800

    Code split editor rendering

--- client/src/components/editor/score/index.jsx
+++ client/src/components/editor/score/index.jsx
@@ -2,54 +2,14 @@
 import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import Vex from 'vexflow';
-
-const VF = Vex.Flow;
+import ScoreRenderer from './renderer';
 
 const Score = () => {
   const container = React.createRef();
   let renderer;
-  let context;
-  let stave;
-  const notes = [];
-  let voice;
-  let formatter;
 
   const initialize = () => {
-    // Create renderer
-    renderer = new VF.Renderer(container.current, VF.Renderer.Backends.SVG);
-
-    // Configure rendering context
-    const contextSVG = renderer.ctx.svg;
-    contextSVG.setAttribute('height', '100%');
-    contextSVG.setAttribute('width', '100%');
-    context = renderer.getContext();
-
-    // Create a stave of width 250 at position 10, 40 on the canvas.
-    stave = new VF.Stave(10, 40, 250);
-
-    // Add a clef and time signature.
-    stave.addClef('treble').addTimeSignature('4/4');
-    stave.setContext(context).draw();
-
-    // Add quarter rests
-    for (let i = 0; i < 4; i += 1) {
-      notes.push(new VF.StaveNote({ clef: 'treble', keys: ['b/4'], duration: 'qr' }));
-    }
-
-    // Create a voice in 4/4 and add above notes
-    voice = new VF.Voice({ num_beats: 4, beat_value: 4 });
-    voice.addTickables(notes);
-
-    // Format and justify the notes to 400 pixels.
-    formatter = new VF.Formatter().joinVoices([voice]).format([voice], 200);
-
-    // Render voice
-    voice.draw(context, stave);
-
-    // Scale svg to fit container
-    const contextWidth = context.attributes.x;
-    const contextHeight = context.attributes.y;
-    context.setViewBox(5, contextHeight - 15, contextWidth, contextHeight);
+    renderer = new ScoreRenderer(container.current);
   };
 
   useEffect(() => {
---
+++ client/src/components/editor/score/renderer.js
@@ -0,0 +1,46 @@
+import Vex from 'vexflow';
+
+const VF = Vex.Flow;
+
+class ScoreRenderer {
+  constructor(containerElement) {
+    this.renderer = new VF.Renderer(containerElement, VF.Renderer.Backends.SVG);
+
+    // Size context svg
+    const contextSVG = this.renderer.ctx.svg;
+    contextSVG.setAttribute('width', '100%');
+    contextSVG.setAttribute('height', '100%');
+
+    this.context = this.renderer.getContext();
+
+    // Create a stave of width 250 at position 10, 40 on the canvas.
+    this.stave = new VF.Stave(10, 40, 250);
+
+    // Add a clef and time signature.
+    this.stave.addClef('treble').addTimeSignature('4/4');
+    this.stave.setContext(this.context).draw();
+
+    // Add quarter rests
+    this.notes = [];
+    for (let i = 0; i < 4; i += 1) {
+      this.notes.push(new VF.StaveNote({ clef: 'treble', keys: ['b/4'], duration: 'qr' }));
+    }
+
+    // Create a voice in 4/4 and add above notes
+    this.voice = new VF.Voice({ num_beats: 4, beat_value: 4 });
+    this.voice.addTickables(this.notes);
+
+    // Format and justify the notes to 400 pixels.
+    this.formatter = new VF.Formatter().joinVoices([this.voice]).format([this.voice], 200);
+
+    // Render voice
+    this.voice.draw(this.context, this.stave);
+
+    // Scale svg to fit container
+    const contextWidth = this.context.attributes.x;
+    const contextHeight = this.context.attributes.y;
+    this.context.setViewBox(5, contextHeight - 15, contextWidth, contextHeight);
+  }
+}
+
+export default ScoreRenderer;
