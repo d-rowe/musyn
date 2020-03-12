a4c1121378bc6aee459b2c6300b3ca5af2f12b7a
Daniel Rowe <danielrowetech@gmail.com>
2020-03-11 22:57:17 -0700

    Migrate x unit from beat to tick

--- client/src/editor/score/models/cursors.js
+++ client/src/editor/score/models/cursors.js
@@ -1,41 +1,41 @@
-/* eslint-disable import/no-named-as-default-member */
 import socket from './socket';
 import view from '../views/score';
 
+// TODO: Refactor to singleton for global document cursor data
 class Cursors {
   constructor() {
     // TODO: update to UUIDs, handle n users
     this.users = {
-      local: { note: null, beatIndex: null, color: 'rgba(51, 101, 138, 0.8)' },
-      remote: { note: null, beatIndex: null, color: 'rgba(242, 100, 25, 0.8)' },
+      local: { note: null, tick: null, color: 'rgba(51, 101, 138, 0.8)' },
+      remote: { note: null, tick: null, color: 'rgba(242, 100, 25, 0.8)' },
     };
 
     // TODO: use this.update()
     socket.on.cursorUpdate = (cursor) => {
       this.users.remote.note = cursor.notename;
-      this.users.remote.beatIndex = cursor.beatIndex;
+      this.users.remote.tick = cursor.tick;
       view.rerender();
     };
 
     // TODO: use this.remove()
     socket.on.cursorRemove = () => {
       this.users.remote.note = null;
-      this.users.remote.beatIndex = null;
+      this.users.remote.tick = null;
       view.rerender();
     };
   }
 
   // TODO: only send if local user
-  update(user, notename, beatIndex) {
+  update(user, notename, tick) {
     this.users[user].note = notename;
-    this.users[user].beatIndex = beatIndex;
-    socket.sendCursorUpdate(notename, beatIndex);
+    this.users[user].tick = tick;
+    socket.sendCursorUpdate(notename, tick);
   }
 
   // TODO: only send if local user
   remove(user) {
     this.users[user].note = null;
-    this.users[user].beatIndex = null;
+    this.users[user].tick = null;
     socket.sendCursorRemove();
   }
 
@@ -45,16 +45,16 @@ class Cursors {
     const output = {};
 
     userIds.forEach((user) => {
-      const { note, color, beatIndex } = this.users[user];
+      const { note, color, tick } = this.users[user];
 
       if (note === null) return;
 
       const userCursor = { user, note, color };
 
-      if (output[beatIndex] === undefined) {
-        output[beatIndex] = [userCursor];
+      if (output[tick] === undefined) {
+        output[tick] = [userCursor];
       } else {
-        output[beatIndex].push(userCursor);
+        output[tick].push(userCursor);
       }
     });
 
