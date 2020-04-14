839b979aa29bdb332797072c464c0b71d73cebc5
Daniel Rowe <danielrowetech@gmail.com>
2020-04-14 15:34:06 -0700

    Add authentication

--- database/index.js
+++ database/index.js
@@ -3,6 +3,7 @@ const { Pool } = require('pg');
 const pool = new Pool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
+  password: process.env.DB_PASS,
   database: process.env.DB_NAME,
 });
 
