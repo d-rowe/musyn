24d07e905c6c6262dd965eb33223ce1318a8f649
Daniel Rowe <danielrowetech@gmail.com>
2020-05-01 18:08:34 -0700

    Index scores.edit_id

--- database/schema.sql
+++ database/schema.sql
@@ -1,17 +1,19 @@
 DROP TABLE IF EXISTS edits, scores;
 
 CREATE TABLE edits (
-  id serial PRIMARY KEY NOT NULL,
-  uuid varchar(15) NOT NULL,
-  action varchar(10) NOT NULL,
-  pitch varchar(3) NOT NULL,
-  measure numeric(3, 0) NOT NULL,
-  start numeric(4, 0) NOT NULL,
-  duration numeric(4, 0)
+  id SERIAL PRIMARY KEY NOT NULL,
+  uuid VARCHAR(15) NOT NULL,
+  action VARCHAR(10) NOT NULL,
+  pitch VARCHAR(3) NOT NULL,
+  measure NUMERIC(3, 0) NOT NULL,
+  start NUMERIC(4, 0) NOT NULL,
+  duration NUMERIC(4, 0)
 );
 
 CREATE TABLE scores (
-  id serial PRIMARY KEY NOT NULL,
-  edit_id serial NOT NULL,
-  score jsonb NOT NULL
+  id SERIAL PRIMARY KEY NOT NULL,
+  edit_id SERIAL NOT NULL,
+  data JSONB NOT NULL
 );
+
+CREATE UNIQUE INDEX edit_idx ON scores (edit_id);
