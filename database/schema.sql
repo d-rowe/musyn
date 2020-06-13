f5d747c683b2c2effafa68fe40a43bbbb06f2c21
Daniel Rowe <danielrowetech@gmail.com>
2020-06-13 14:00:29 -0700

    Add references

--- database/schema.sql
+++ database/schema.sql
@@ -1,8 +1,17 @@
 DROP TABLE IF EXISTS edits, score_cache, compositions;
 
+CREATE TABLE compositions (
+  id SERIAL PRIMARY KEY NOT NULL,
+  title VARCHAR NOT NULL,
+  hash VARCHAR NOT NULL,
+  version INTEGER NOT NULL
+);
+
+CREATE UNIQUE INDEX ON compositions(hash);
+
 CREATE TABLE edits (
   id SERIAL PRIMARY KEY NOT NULL,
-  composition_id INTEGER,
+  composition_id INTEGER REFERENCES compositions(id),
   uuid VARCHAR NOT NULL,
   action VARCHAR NOT NULL,
   pitch VARCHAR NOT NULL,
@@ -15,19 +24,10 @@ CREATE INDEX ON edits(composition_id);
 
 CREATE TABLE score_cache (
   id SERIAL PRIMARY KEY NOT NULL,
-  composition_id INTEGER,
-  edit_id INTEGER NOT NULL,
+  composition_id INTEGER REFERENCES compositions(id),
+  edit_id INTEGER NOT NULL REFERENCES edits(id),
   score JSONB NOT NULL
 );
 
 CREATE UNIQUE INDEX ON score_cache(edit_id);
 CREATE UNIQUE INDEX ON score_cache(composition_id);
-
-CREATE TABLE compositions (
-  id SERIAL PRIMARY KEY NOT NULL,
-  title VARCHAR NOT NULL,
-  hash VARCHAR NOT NULL,
-  version INTEGER NOT NULL
-);
-
-CREATE UNIQUE INDEX ON compositions(hash);
