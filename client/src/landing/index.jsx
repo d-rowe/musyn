7bf53bf5561f6a76c20a95117d4fa19bc93164d3
Daniel Rowe <danielrowetech@gmail.com>
2020-06-17 16:41:05 -0700

    Disable join button until valid 32 char code

--- client/src/landing/index.jsx
+++ client/src/landing/index.jsx
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
 const Landing = () => {
+  const [compositionHash, setCompositionHash] = useState('');
+  const [validHash, setValidHash] = useState(false);
+
+  const redirectToHash = (hash) => {
+    window.location = `/compositions/${hash}`;
+  };
+
   const createComposition = () => {
     axios.post('/api/compositions')
       .then((response) => response.data)
-      .then(({ hash }) => { window.location = `/compositions/${hash}`; })
+      .then(({ hash }) => redirectToHash(hash))
       .catch((err) => {
         // eslint-disable-next-line no-console
         console.warn(err);
         // eslint-disable-next-line no-alert
         alert('Encountered problem creating composition');
       });
+  };
+
+  const hashUpdate = (e) => {
+    const hash = e.target.value;
+    setCompositionHash(hash);
+    setValidHash(hash.length === 32);
   };
 
   return (
@@ -24,7 +37,21 @@ const Landing = () => {
       >
         Create Composition
       </button>
-      <button type="button" className="button">Join Composition</button>
+      <div className="field has-addons">
+        <div className="control">
+          <input className="input" type="text" placeholder="Composition code" maxLength="32" onChange={hashUpdate} />
+        </div>
+        <div className="control">
+          <button
+            type="button"
+            className="button is-info"
+            onClick={() => redirectToHash(compositionHash)}
+            disabled={!validHash}
+          >
+            Join Composition
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
