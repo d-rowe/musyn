5412b01d633c57fb89524cdf4ac407f02c385be6
Daniel Rowe <danielrowetech@gmail.com>
2020-06-17 15:33:56 -0700

    Add basic create composition button

--- client/src/landing/index.jsx
+++ client/src/landing/index.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import axios from 'axios';
 
-const Landing = () => (
-  <div>
-    <h1 className="title">Welcome to Musyn</h1>
-  </div>
-);
+const Landing = () => {
+  const createComposition = () => {
+    axios.post('/api/compositions')
+      .then((response) => response.data)
+      .then(({ hash }) => { window.location = `/compositions/${hash}`; })
+      .catch((err) => {
+        // eslint-disable-next-line no-console
+        console.warn(err);
+        // eslint-disable-next-line no-alert
+        alert('Encountered problem creating composition');
+      });
+  };
+
+  return (
+    <div>
+      <h1 className="title">Welcome to Musyn</h1>
+      <button
+        type="button"
+        className="button"
+        onClick={createComposition}
+      >
+        Create Composition
+      </button>
+      <button type="button" className="button">Join Composition</button>
+    </div>
+  );
+};
 
 export default Landing;
