c5521a037599066279f7a1a50ae531475c83c17b
Daniel Rowe <danielrowetech@gmail.com>
2020-06-19 17:33:49 -0700

    Remove unnecessary import

--- client/src/components/landing/compositionCounter.jsx
+++ client/src/components/landing/compositionCounter.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import styled from 'styled-components';
 
 const CompositionCounter = () => {
   const [count, setCount] = useState('');
