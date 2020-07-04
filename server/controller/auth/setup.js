0f44e012c19d5536a7583d04c8272269e6aba312
Daniel Rowe <danielrowetech@gmail.com>
2020-07-04 15:34:15 -0700

    Register users on signin

--- server/controller/auth/setup.js
+++ server/controller/auth/setup.js
@@ -1,12 +1,32 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
+const User = require('../../entity/user');
 
 passport.use(
   new GoogleStrategy({
     callbackURL: '/auth/google/redirect',
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  }, () => {
-    // callback
+  }, async (accessToken, refreshToken, profile, done) => {
+    const {
+      id,
+      displayName,
+      name: {
+        givenName, familyName,
+      },
+    } = profile;
+
+    const hasPrevRegistered = await User.exists('google', id);
+
+    if (!hasPrevRegistered) {
+      // eslint-disable-next-line no-console
+      console.log('Registering', displayName);
+      await User.register(displayName, givenName, familyName, id, 'google');
+    } else {
+      // eslint-disable-next-line no-console
+      console.log(displayName, 'already registered');
+    }
+
+    done();
   }),
 );
