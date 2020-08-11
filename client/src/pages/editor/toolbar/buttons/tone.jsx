a5344ce0644d6934465430c94bb291970c993de1
Dustin Morris <dustinnuc@gmail.com>
2020-08-10 20:05:49 -0700

    Add Tone Selector Button to UI
    Because:
    - Multiple instrument tones can be selected
    - Will open a circular menu selector for instrument tone
    
    Adding a button to the UI will open a floating circular menu with icons
    that allow the user to select different tones.

---
+++ client/src/pages/editor/toolbar/buttons/tone.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const imgLocation = '/assets/img/';
+const imgExtension = 'svg';
+
+const ToneButton = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <Button className='button is-light' onClick = {() => onClick()} img = 'piano_keys' >
+    </Button>
+  )
+}
+
+const Button = styled.button`
+  width: 2em;
+  height: 2em;
+  padding: 0.25em;
+  margin: 0.25em;
+  background-image: url(${imgLocation}${(props) => props.img}.${imgExtension});
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: contain;
+`;
+
+export default ToneButton;
